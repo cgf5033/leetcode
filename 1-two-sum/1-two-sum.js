a0af16ab555bf1e747b8c6f7e6b00f8dec37e2b2
Colin Fitzhenry <cgfitzhenry@gmail.com>
2022-05-31 12:15:14 -0400

    Time: 71 ms (89.76%), Space: 42.7 MB (51.44%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let memo = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        if (memo[target - nums[i]] !== undefined) {
+            return [i, memo[target - nums[i]]];
         }
+        memo[nums[i]] = i;
     }
 };
