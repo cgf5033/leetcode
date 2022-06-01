109b20f57e831da5c35243f5f7a1f0fa2665a6bf
Colin Fitzhenry <cgfitzhenry@gmail.com>
2022-06-01 12:07:31 -0400

    Time: 94 ms (67.23%), Space: 42.6 MB (51.37%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // create an empty object
     let memo = {};
+    // loop over the nums array
     for (let i = 0; i < nums.length; i++) {
-        if (memo[target - nums[i]] !== undefined) {
-            return [i, memo[target - nums[i]]];
+        // if target - number at current postion is in the object
+        let numberNeeded = target - nums[i];
+        if (memo[numberNeeded] !== undefined) {
+            return [i, memo[numberNeeded]];
         }
         memo[nums[i]] = i;
+            // return [i, object[target - number[i]]]
+        // add to object -> key=number at index, value = index(i)
     }
 };
+
+// i: 
+//     nums: array of integers (neg or pos)
+//     target: integer (neg or pos)
+// o: array of indices (add up to target)
+// c: cant use same number twice
+// e: none
